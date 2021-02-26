23173ad36be1a4b001668d1a8394f808b0be931a
amie senghore <amiesenghore1@gmail.com>
2021-02-26 11:55:52 +0100

    exercise1js

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,13 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+function injectWords(){
+    let noun=prompt("enter noun");
+    let verb=prompt("enter verb");
+    let adjective=prompt("enter adjective");
+    let adverb=prompt("enter adverb");
+    window.alert(noun + " " + verb + " " + adjective + " she " + verb +  " doing " + adverb)
+
+}
+injectWords()
